8ffba07ed62163361c4f966c32248d06da7c2db1
Ben Moss <benm@vmware.com>
2022-05-05 10:28:52 -0400

    add back kapp_controller_version

--- hack/deploy.sh
+++ hack/deploy.sh
@@ -4,7 +4,7 @@ set -e
 
 docker build -t ko.local/kc-base -q .
 export VERSION=develop
-ytt -f config -v kapp_controller_version="$VERSION" | ko resolve -Pf- | kapp deploy -a kc -f- -c -y
+ytt -f config | ko resolve -Pf- | kapp deploy -a kc -f- -c -y
 
 source ./hack/secretgen-controller.sh
 deploy_secretgen-controller
