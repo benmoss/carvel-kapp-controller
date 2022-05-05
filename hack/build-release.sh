ee88c969db577bc5a81155349fb9a7890e57dd9e
Ben Moss <benm@vmware.com>
2022-05-05 15:06:23 -0400

    fixamaroo

--- hack/build-release.sh
+++ hack/build-release.sh
@@ -9,7 +9,7 @@ source $(dirname "$0")/version-util.sh
 
 docker build -t ko.local/kc-base -q --pull .
 export VERSION="$(get_kappctrl_ver)"
-ytt -f config/ -v kapp_controller_version="$VERSION" | ko resolve -Pf- > ./tmp/release.yml
+ytt -f config/ -v kapp_controller_version="$VERSION" | ko resolve -f- > ./tmp/release.yml
 
 shasum -a 256 ./tmp/release*.yml | tee ./tmp/checksums.txt
 
--- hack/deploy.sh
+++ hack/deploy.sh
@@ -3,7 +3,7 @@
 set -e
 
 docker build -t ko.local/kc-base -q .
-ytt -f config | ko resolve -Pf- | kapp deploy -a kc -f- -c -y
+ytt -f config | ko resolve -f- | kapp deploy -a kc -f- -c -y
 
 source ./hack/secretgen-controller.sh
 deploy_secretgen-controller
