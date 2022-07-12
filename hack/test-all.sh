eb8929e8ea2e8bdbaf6da0bdfb8f17a6af22c2f4
Ben Moss <benm@vmware.com>
2022-07-11 20:07:20 -0400

    try this

--- hack/test-all.sh
+++ hack/test-all.sh
@@ -2,13 +2,10 @@
 
 set -e -x -u
 
+bash -c "while true; do ulimit -u; sleep 2; done" &
 ./hack/build.sh
-echo ulimit: $(ulimit -u)
 ./hack/test.sh
-echo ulimit: $(ulimit -u)
 ./hack/test-e2e.sh
-echo ulimit: $(ulimit -u)
 ./hack/test-examples.sh
-echo ulimit: $(ulimit -u)
 
 echo ALL SUCCESS
